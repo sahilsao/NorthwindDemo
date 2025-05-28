462d43319f9e26d6618e0eeecb0fe5672a3e5e1b
sahilsao <sahilsaocs@gmail.com>
2025-05-28 14:29:22 +0530

    fix

--- NorthwindDemo/Program.cs
+++ NorthwindDemo/Program.cs
@@ -12,9 +12,6 @@ builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddScoped<CookieService>();
-
 builder.Services.AddDbContext<DbContextNorthwind>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
